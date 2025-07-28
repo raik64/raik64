5b0ba33d6e353d4b2d76c87e06f511cc00ad1adb
raik64 <raikenta64@gmail.com>
2025-07-27 22:32:07 -0500

    Initial Commit

---
+++ SitiosTuristicos.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+  <title>Sitios Turísticos-Francisco de Orellana</title>
+  <body style="padding: 0%; margin: 0; background-color:#96d288;">
+
+  <center>
+    <div style="background-color: rgb(208, 255, 0); width: 1450px; height: 400px; border-radius: 16px;">
+      
+      <div style="background-color:#000000; width: 1430px; height: 100px; border-radius: 16px; padding: 10px; text-align: center;">
+        <ul style="padding: 0; list-style: none;">
+          <li style="font-size:35px; font-family:Verdana; display:inline; margin:20px;">
+            <a href="index.html" style="color:#19d01c;">Home</a>
+          </li>
+          <li style="font-size:35px; font-family:Verdana; display:inline; margin:20px;">
+            <a href="SitiosGeograficos.html" style="color:#FFFF00;">Sitios Geograficos</a>
+          </li>
+          <li style="font-size:35px; font-family:Verdana; display:inline; margin:20px;">
+            <a href="Costumbre.html" style="color:#19d01c;">Costumbre</a>
+          </li>
+          <li style="font-size:35px; font-family:Verdana; display:inline; margin:20px;">
+            <a href="Galeria.html" style="color:#FFFF00;">Galeria</a>
+          </li>
+          <li style="font-size:35px; font-family:Verdana; display:inline; margin:20px;">
+            <a href="SitiosTuristicos.html" style="color:#19d01c;">Sitios Turisticos</a>
+          </li>
+        </ul>
+      </div>
+    <div>
+  <img src="Francisco de Orellana.gif" width="1450px" height="400px">
+  </center>
+  </div>
